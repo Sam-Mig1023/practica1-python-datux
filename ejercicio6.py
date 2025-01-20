977a3343246c888399d80a90f8609644ef370f14
Sam-Mig1023 <samira1reyna1@gmail.com>
2025-01-20 05:41:57 +0000

    tarea

---
+++ ejercicio6.py
@@ -0,0 +1,5 @@
+edad=int(input("Ingrese su edad: "))
+if edad>=18:
+    print("La persona es mayor de edad.")
+else:
+    print("La persona no es mayor de edad.")
---
+++ ejercicio8.py
@@ -0,0 +1,3 @@
+numero=int(input("Ingrese un numero entero: "))
+suma=(numero*(numero+1))/2
+print(f"La suma de los {numero} primeros numeros es {suma}.")
